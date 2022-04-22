791855deda8bfe7af50337bf63433304cc3c6c2b
Lee <jessy1092@gmail.com>
2022-04-23 00:34:18 +0800

    feat: Implement Input component

---
+++ src/components/atoms/Input/__tests__/Input.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { boolean } from '@storybook/addon-knobs/react';
+
+import Input, { InputSearch } from 'components/atoms/Input';
+
+const stories = storiesOf('atoms/Input', module);
+
+stories.add('__interactive', () => (
+	<Input
+		disabled={boolean('disabled', false)}
+		error={boolean('error', false)}
+		placeholder="請輸入"
+	/>
+));
+
+stories.add('with search input', () => <InputSearch placeholder="請輸入" />);
---
+++ src/components/atoms/Input/index.js
@@ -0,0 +1,44 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+import React from 'react';
+import classnames from 'classnames';
+
+// import EyeIcon from 'images/icon/eye-show.inline.svg';
+// import EyeHideIcon from 'images/icon/eye-hide.inline.svg';
+import SearchIcon from 'images/icon/search.inline.svg';
+
+import { isExist } from 'util/helper';
+
+import styles from './index.css';
+
+export const InputSearch = ({ ...other }) => (
+	<Input {...other} type="text" Icon={SearchIcon} isLeftIcon />
+);
+
+const Input = ({
+	className,
+	Icon,
+	isLeftIcon,
+	onClickIcon = () => {},
+	disabled = false,
+	error = false,
+	...other
+}) => (
+	<div
+		className={classnames(styles.input, className, {
+			[styles.icon]: isExist(Icon),
+			[styles.isLeftIcon]: isExist(Icon) && isLeftIcon,
+			[styles.disabled]: disabled,
+			[styles.error]: error,
+		})}
+	>
+		<input disabled={disabled} {...other} />
+		{isExist(Icon) && (
+			<button className={styles.icon} onClick={onClickIcon} type="button">
+				<Icon />
+			</button>
+		)}
+	</div>
+);
+
+export default Input;
