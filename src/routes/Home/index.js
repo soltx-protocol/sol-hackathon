e288b9a14e932b003a9cb57f3f938475a59db43d
Lee <jessy1092@gmail.com>
2022-04-22 18:47:40 +0800

    fix: Change display pool

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -7,8 +7,8 @@ import styles from './index.css';
 const Home = () => (
 	<div className={styles.home}>
 		<DuneTxPool />
-		<DuneTxPool />
-		<DuneTxPool />
+		<DuneTxPool poolAccount="EuK3xDa4rWuHeMQCBsHf1ETZNiEQb5C476oE9u9kp8Ji" />
+		<DuneTxPool poolAccount="FgZut2qVQEyPBibaTJbbX2PxaMZvT1vjDebiVaDp5BWP" />
 	</div>
 );
 
