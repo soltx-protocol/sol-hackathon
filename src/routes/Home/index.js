f671882645659b254e1f1cd827dd4e5a65f49bea
Lee <jessy1092@gmail.com>
2022-04-23 15:30:15 +0800

    feat: Implement the Overall page

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,14 +1,36 @@
+import Button from 'components/atoms/Button';
 import DuneTxPool from 'components/molecules/DuneTxPool';
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
+import KingIcon from 'images/icon/king.inline.svg';
+
+import Input, { InputSearch } from 'components/atoms/Input';
 import styles from './index.css';
 
 const Home = () => (
 	<div className={styles.home}>
-		<DuneTxPool />
-		<DuneTxPool poolAccount="EuK3xDa4rWuHeMQCBsHf1ETZNiEQb5C476oE9u9kp8Ji" />
-		<DuneTxPool poolAccount="FgZut2qVQEyPBibaTJbbX2PxaMZvT1vjDebiVaDp5BWP" />
+		<div className={styles.top}>
+			<div className={styles.king}>
+				<KingIcon />
+				<div className={styles.description}>{'Top Asset Owner\nToday'}</div>
+			</div>
+			<div className={styles.wallet}>
+				<Input className={styles.input} placeholder="Insert wallet address" />
+				<Button>Generate</Button>
+			</div>
+		</div>
+
+		<div className={styles.search}>
+			<div className={styles.title}>Pool Search</div>
+			<InputSearch />
+		</div>
+		<div className={styles.header}>Overall</div>
+		<div className={styles.content}>
+			<DuneTxPool />
+			<DuneTxPool poolAccount="EuK3xDa4rWuHeMQCBsHf1ETZNiEQb5C476oE9u9kp8Ji" />
+			<DuneTxPool poolAccount="FgZut2qVQEyPBibaTJbbX2PxaMZvT1vjDebiVaDp5BWP" />
+		</div>
 	</div>
 );
 
