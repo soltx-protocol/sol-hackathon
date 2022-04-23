c608fadc350a353be97ee7514384c635ec56cc45
Lee <jessy1092@gmail.com>
2022-04-23 14:35:59 +0800

    feat: Update Navbar style

--- src/components/molecules/Navbar/index.js
+++ src/components/molecules/Navbar/index.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import classnames from 'classnames';
 
-import LogoIcon from 'images/icon/logo.inline.svg';
+import OrcaLogo from 'images/orca.png';
+import SoltxLogo from 'images/icon/soltx.inline.svg';
 import TradeIcon from 'images/icon/trade.inline.svg';
 import TradeActiveIcon from 'images/icon/trade-active.inline.svg';
+import WalletIcon from 'images/icon/wallet.inline.svg';
+import WalletActiveIcon from 'images/icon/wallet-active.inline.svg';
 
 import styles from './index.css';
 
 export const NAV_SELECT = {
-	DASHBOARD: 'DASHBOARD',
+	OVERALL: 'OVERALL',
+	WALLET: 'WALLET',
 	NONE: 'NONE',
 };
 
 const Navbar = ({ className, select = NAV_SELECT.NONE }) => (
 	<div className={classnames(styles.navbar, className)}>
 		<div className={styles.icon}>
-			<LogoIcon />
+			<img src={OrcaLogo} alt="orca" /> Hackboard
 		</div>
 		<button
-			className={classnames({ [styles.active]: select === NAV_SELECT.DASHBOARD })}
+			className={classnames({ [styles.active]: select === NAV_SELECT.OVERALL })}
 			type="button"
 		>
-			<div>{select === NAV_SELECT.DASHBOARD ? <TradeActiveIcon /> : <TradeIcon />} Dashboard</div>
+			<div>{select === NAV_SELECT.OVERALL ? <TradeActiveIcon /> : <TradeIcon />} Overall</div>
 		</button>
+		<button className={classnames({ [styles.active]: select === NAV_SELECT.WALLET })} type="button">
+			<div>
+				{select === NAV_SELECT.WALLET ? <WalletActiveIcon /> : <WalletIcon />} Individual Wallet
+			</div>
+		</button>
+		<div className={styles.bottom}>
+			<SoltxLogo />
+			Created by Soltx
+		</div>
 	</div>
 );
 
